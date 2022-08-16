af2342308cc9e69db784513b92f27d827467a778
i8lex <cybeerid@gmail.com>
2022-08-16 18:15:34 +0200

    do work with stop after fail

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
-const firstNumber = +(prompt('Enter first number: '));
+
+for (let counter = 1; counter <=1; counter +=1) {
+
+    const firstNumber = +(prompt('Enter first number: '));
 
     if (isNaN(firstNumber)) {
-        alert("Not a number");
-        
+        alert("It's not a number");
+        break;
     }
 
-const operator = prompt('Enter operator (+, -, * or / ): ');
+    let operator = prompt('Enter operator (+, -, * or / ): ');
 
-const secondNumber = +(prompt('Enter second number: '));
-    if (isNaN(firstNumber)) {
-        alert("Not a number");
-    }
+    const secondNumber = +(prompt('Enter second number: '));
+        if (isNaN(firstNumber)) {
+            alert("It's not a number");
+            break;
+        }
 
-let calculation;
+    let calculation;
 
-    if (operator === '+') {
-        calculation = firstNumber + secondNumber;
-    }
+        if (operator === '+') {
+            calculation = firstNumber + secondNumber;
+        }
 
-    else if (operator === '-') {
-        calculation = firstNumber - secondNumber;
-    }
+        else if (operator === '-') {
+            calculation = firstNumber - secondNumber;
+        }
 
-    else if (operator === '*') {
-        calculation = firstNumber * secondNumber;
-    }
+        else if (operator === '*') {
+            calculation = firstNumber * secondNumber;
+        }
 
-    else if (operator === '/') {
-        calculation = firstNumber / secondNumber;
-    }
+        else if (operator === '/') {
+            calculation = firstNumber / secondNumber;
+        }
 
-    else {
-        calculation = "This operation is not exist";
-    }
+        else {
+            calculation = "This operation is not exist";
+        }
 
-let message = (`Your result is "${calculation}"`)
+    let message = (`Your result is "${calculation}"`)
 
-alert(message);
+    alert(message);
+
+}
